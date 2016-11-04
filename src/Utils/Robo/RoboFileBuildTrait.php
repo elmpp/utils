0862a86dfd5b361527c7d92e55d15d325a212ea9
matt penrice <matthew.penrice@gmail.com>
2016-11-04 08:54:46 +0000

    Creates skeleton of project and includes Robo Traits

---
+++ src/Utils/Robo/RoboFileBuildTrait.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Partridge\Utils\Robo;
+
+/**
+ * Segregates the robo tasks concerned with packaging and building of repo projects
+ */
+trait RoboFileBuildTrait {
+
+
+  /**
+   * Dev task to merge Dirty branch into Dev and handle additional things such as versioning numbers
+   *  - merges Dirty into Dev
+   *  - bumps the semver patch number. Will be of the form MAJOR.MINOR.PATCH
+   *  (to bump major/minor, a separate commit should be done beforehand)
+   *
+   */
+  public function buildMergeDev() {
+
+    if ($this->getCurrentGitBranch() != 'dirty') {
+      throw new \Exception("You must be on the branch 'dirty'");
+    }
+
+    $result = $this->taskSemVer('.semver')
+                   ->increment('patch')
+                   ->run()
+    ;
+    if (!$result->wasSuccessful()) {
+      throw new \Exception("Bad semver file");
+    }
+
+    return $this->taskGitStack()
+                ->stopOnFail(true)
+                ->add('.semver')
+                ->commit("Bumps .semver")
+                ->push()
+                ->checkout('dev')
+                ->merge('dirty')
+                ->push()
+                ->checkout('dirty')
+                ->run()
+      ;
+  }
+
+  /**
+   * Uses quicker method to create the image. Should not be used on CI server however
+   * @param srting $imageKey (importer|api)
+   * @param string $mainTag
+   * @return mixed
+   */
+  protected function packageProject($packageType, $mainTag = 'latest-dev') {
+
+    $packagerContainer = 'packager-' . $packageType;
+
+    if ($packageType == 'api') {
+      $baseImage        = $this->getDefaultRegistry() . '/' . $this->imageNames['php-apache'] . ':latest';
+      $containerDocRoot = '/var/www/html';
+      $containerCmd     = 'CMD ["apache2-foreground"]';
+      $runAsUser        = 'www-data';
+    }
+    elseif ($packageType == 'importer') {
+      $baseImage        = $this->getDefaultRegistry() . '/' . $this->imageNames['php-cli'] . ':latest';
+      $containerDocRoot = '/opt/app';
+      $containerCmd     = 'CMD ["php", "-a"]';
+      $runAsUser        = 'root';
+    }
+    elseif ($packageType == 'frontend') {
+      $baseImage        = $this->getDefaultRegistry() . '/' . $this->imageNames['node'] . ':latest';
+      $containerDocRoot = '/var/www';
+      $containerCmd     = 'CMD ["node"]';
+      $runAsUser        = 'root';
+    }
+
+    $srcCodeLoc = $this->getCurrentProjectDir();
+    $semVer     = (string) (new \Robo\Task\Development\SemVer($srcCodeLoc . "/.semver"));
+
+    $this->say("Using base image: ${baseImage}");
+
+    $registry   = $this->getDefaultRegistry() . '/' . $this->imageNames[$packageType];
+    $registrySemver  = $registry . ':' . $semVer;
+    $registryMain    = $registry . ':' . $mainTag;
+
+    $collection = $this->getCollectionForPackaging(
+      $packagerContainer,
+      $baseImage,
+      $srcCodeLoc,
+      $containerDocRoot,
+      $registrySemver,
+      $registryMain,
+      $runAsUser,
+      $containerCmd
+    );
+
+    return $collection
+      ->run()
+      ->stopOnFail()
+    ;
+  }
+}
---
+++ src/Utils/Robo/RoboFileTrait.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Partridge\Utils\Robo;
+
+trait RoboFileTrait {
+
+  protected $registries = [];
+  protected $imageNames = [
+    // packaged (we makey)
+    'api'        => 'api',
+    'importer'   => 'importer',
+    'frontend'   => 'frontend',
+    // base
+    'php-cli'    => 'php-cli',
+    'php-apache' => 'php-apache',
+    'node'       => 'node',
+  ];
+
+
+  protected function setupRegistries() {
+
+
+    if (!isset($_SERVER['GCR_PROJECT_ID'])) {
+      throw new \Exception("\"GCR_PROJECT_ID\" must be set in environment pointing at relevant GCR project");
+    }
+    $this->registries[self::REGISTRY_GCR]   = "eu.gcr.io/" . $_SERVER['GCR_PROJECT_ID'];
+    $this->registries[self::REGISTRY_LOCAL] = "localhost:5000";
+  }
+
+  /**
+   * Does a docker push
+   * @param string $fullTag
+   * @param string $registry
+   */
+  public function imagePush($fullTag) {
+
+    $binary = (false !== stristr($fullTag, 'gcr.io')) ? 'gcloud docker' : 'docker';
+    $this->taskExec("${binary} push $fullTag")
+         ->printed(true)
+         ->run()
+    ;
+  }
+
+  /**
+   * Does a docker pull
+   * @param string $fullTag
+   * @param string $registry
+   */
+  public function imagePull($fullTag) {
+
+    $binary = (false !== stristr($fullTag, 'gcr.io')) ? 'gcloud docker' : 'docker';
+    $this->taskExec("${binary} pull $fullTag")
+         ->printed(true)
+         ->run()
+    ;
+  }
+
+  /**
+   * Grabs a file from BitBucket using wget. Hides the authorisation a little also
+   * @param $url
+   * @param $targetLoc
+   */
+  protected function bitbucketDownload($hash, $targetLoc) {
+
+    if (!is_file($targetLoc) || (ctype_alpha($hash))) { // only overwrite branch references
+      $res = $this
+        ->taskExec("git archive --remote=ssh://git@bitbucket.org/alanpartridge/api.git --format=tar.gz --output=${targetLoc} ${hash}")
+        ->printed(true)
+        ->run()
+      ;
+      if (!$res->wasSuccessful()) {
+        $sshKey = $_SERVER['HOME'] . '/.ssh/id_rsa.pub';
+        if (!is_readable($sshKey)) {
+          $this->yell("Add a ${sshKey} key and authorise with bitbucket", 40, 'red');
+        }
+        else {
+          $this->say("File ${sshKey} was found despite fetch failure");
+        }
+        exit(1);
+      }
+      return $res;
+    }
+  }
+
+  /**
+   * Selects the best registry for docker image actions. Will return localhost if looks like a dev machine
+   * @return string
+   */
+  protected function getDefaultRegistry() {
+
+    if ($this->isLocalDevMachine()) {
+      return $this->registries[self::REGISTRY_LOCAL];
+    }
+    return $this->registries[self::REGISTRY_GCR];
+  }
+
+  /**
+   * Includes cross-project can be confusing
+   * @return string
+   */
+  protected function getCurrentProjectDir() {
+
+    return substr(realpath($_SERVER["SCRIPT_FILENAME"]), 0, stripos(realpath($_SERVER["SCRIPT_FILENAME"]), '/robo'));
+  }
+
+  protected function isLocalDevMachine() {
+    return (false !== stristr(gethostname(), 'Matthews-iMac') || false !== stristr(gethostname(), 'matts-MacBook'));
+  }
+
+  protected function getCurrentGitBranch() {
+
+    $currentBranch = $this->taskExec('git rev-parse --abbrev-ref HEAD')->run();
+    $currentBranch = trim($currentBranch->getOutputData(), PHP_EOL);
+    return $currentBranch;
+  }
+
+
+}
