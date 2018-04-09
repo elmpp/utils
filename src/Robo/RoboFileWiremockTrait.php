69f6783040c56ecaefb83f405694fd393e658048
Matt Penrice <matthew.penrice@gmail.com>
2018-04-09 16:06:31 +0100

    wiremock backgrounding

--- src/Robo/RoboFileWiremockTrait.php
+++ src/Robo/RoboFileWiremockTrait.php
@@ -96,8 +96,8 @@ trait RoboFileWiremockTrait
         if ($background) {
             $coll
             ->background()
-            ->idleTimeout(2)
-//            ->rawArg('&> /tmp/wiremock || true')     // don't know why need to force the true now with robo but whatevs
+//            ->idleTimeout(2)
+            ->rawArg('&> /tmp/wiremock || true')     // don't know why need to force the true now with robo but whatevs
 //            ->rawArg('&>')     // don't know why need to force the true now with robo but whatevs
             ;
         }
